1c9b6e152b21cc35a0cbd269bbbb21ff9dde7c54
Abdullahi Lawal <abdullahilawalmusa477@gmail.com>
2026-04-19 15:01:54 +0100

    ui: add class creation prerequisite note to student management

--- client/src/pages/admin/StudentManagement.jsx
+++ client/src/pages/admin/StudentManagement.jsx
@@ -855,6 +855,12 @@ Note: Password must be changed on first login.
                 </p>
               </div>
             </div>
+            <p className="mt-4 text-[10px] font-black uppercase tracking-widest text-red-600 flex items-center gap-2">
+              <svg className="w-3 h-3" fill="currentColor" viewBox="0 0 20 20">
+                <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+              </svg>
+              Note: Classes must be created before students can be added 
+            </p>
           </div>
         </div>
       </div>
